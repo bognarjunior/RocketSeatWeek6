c46f9582531674d5e8eab8079eeb9c5ad1f049fa
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-10 18:37:29 -0300

    Create model File
    Criando o model File

--- backend/src/models/Box.js
+++ backend/src/models/Box.js
@@ -5,7 +5,10 @@ const Box = new mongoose.Schema({
 		type: String,
 		required: true
 	},
-	files: []
+	files: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'File'
+	}]
 }, {
 	timestamps: true
 });
